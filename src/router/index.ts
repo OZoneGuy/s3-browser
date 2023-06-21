be0cab88f7117bb9eab14aef66c60677596dbc6e
OZoneGuy <oalkersh@protonmail.com>
2023-06-20 20:47:41 -0400

    Added router to the main page
    Now should get the correct values and render the correct components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DirNavigator from '../views/DirNavigator.vue'
+import Download from '../views/Download.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/:path*',
+      path: '/:dir*',
       name: 'dir',
       component: DirNavigator
     },
     {
-      path: '/download/:path+',
+      path: '/download/:dir*',
       name: 'download',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Download.vue')
+      component: Download
     }
   ]
 })
