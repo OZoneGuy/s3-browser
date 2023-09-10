2aa853b27749e40005e51aa6bbc7fe83b04e48d8
OZoneGuy <oalkersh@protonmail.com>
2023-09-09 22:25:56 -0400

    feat - Added login page and the auth guard to the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DirNavigator from '../views/DirNavigator.vue'
 import Download from '../views/Download.vue'
+import Login from '../views/Login.vue'
+import { isAuthenticated } from '../services/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,8 +19,21 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: Download
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
+})
+
+router.beforeEach(async (to) => {
+  if (!(await isAuthenticated()) && to.name !== 'Login') {
+    return { name: 'Login' }
+  } else {
+    return true
+  }
 })
 
 export default router
